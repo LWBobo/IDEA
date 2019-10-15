7bc1052d7a154535a9439040fced0f089ad78d5d
qingyishi <1697668797@qq.com>
2019-10-15 22:20:56 +0800

    xiugai

--- Demo1/src/Demo1.java
+++ Demo1/src/Demo1.java
@@ -1,5 +1,9 @@
 public class Demo1 {
     public static  void main(String [] args ) {
+
         System.out.println("uyi安全违法侵权法话费");
+        for(int i = 1 ; i < 5 ;i++){
+            System.out.println("uyi安全违法侵权法话费");
+        }
     }
 }
