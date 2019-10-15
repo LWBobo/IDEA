cae9cd3ee3950e4a314a72734d9b570afc531f09
qingyishi <1697668797@qq.com>
2019-10-15 22:18:13 +0800

    1

---
+++ Demo1/src/Demo1.java
@@ -0,0 +1,5 @@
+public class Demo1 {
+    public static  void main(String [] args ) {
+        System.out.println("uyi安全违法侵权法话费");
+    }
+}
---
+++ Demo2/src/Demo2.java
@@ -0,0 +1,8 @@
+public class Demo2 {
+    public static void main(String [] args){
+
+        for(int i = 0 ; i < 5 ; i++){
+            System.out.println("有多少分公司个电话费");
+        }
+    }
+}
