b08bf92601624e71905a775d4c0f406d1173b82b
qingyishi <1697668797@qq.com>
2019-11-25 11:56:00 +0800

    1

--- Demo1/src/Demo1.java
+++
@@ -1,9 +0,0 @@
-public class Demo1 {
-    public static  void main(String [] args ) {
-
-        System.out.println("uyi安全违法侵权法话费");
-        for(int i = 1 ; i < 5 ;i++){
-            System.out.println("uyi安全违法侵权法话费");
-        }
-    }
-}
--- Demo2/src/Demo2.java
+++
@@ -1,9 +0,0 @@
-public class Demo2 {
-    public static void main(String [] args){
-
-        for(int i = 0 ; i < 5 ; i++){
-            System.out.println("有多少分公司个电话费");
-            System.out.println("有多少分公司个电话费");
-        }
-    }
-}
